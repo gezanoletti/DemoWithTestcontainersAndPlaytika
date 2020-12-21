5bd19a7a739c7db431bca0d6f3a94b3f7bffb768
gezanoletti <gezanoletti@gmail.com>
2020-12-21 18:36:05 +0100

    Demo project for TestcontainersWithPlaytika

---
+++ src/main/java/com/gezanoletti/demo/TestcontainersWithPlaytika/TestcontainersWithPlaytikaApplication.java
@@ -0,0 +1,13 @@
+package com.gezanoletti.demo.TestcontainersWithPlaytika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcontainersWithPlaytikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcontainersWithPlaytikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gezanoletti/demo/TestcontainersWithPlaytika/TestcontainersWithPlaytikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gezanoletti.demo.TestcontainersWithPlaytika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersWithPlaytikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
